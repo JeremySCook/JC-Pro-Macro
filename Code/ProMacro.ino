2a9a4a3664ae3304171396bf1720924a5ddb9a4b
Jeremy S Cook <jscook55@gmail.com>
2021-08-11 16:19:04 -0400

    Update ProMacro.ino

--- Code/ProMacro.ino
+++ Code/ProMacro.ino
@@ -28,13 +28,14 @@ long newPosition;
 int inputMode = 0;
 int LEDLight = 1;
 int LEDCircle[4] = {0, 1, 3, 2};
+int counter = 0;
+bool nascar = 0;
+long newNascarTurnTime = 0;
+long oldNascarTurnTime = 0;
 
-//Long Press Setup==================================================
-
-const int LONG_PRESS_TIME  = 500; // 1000 milliseconds
-int lastState = 0;  // the previous state from the input pin
-unsigned long pressedTime  = 0;
-unsigned long releasedTime = 0;
+int modeArray[] = {0, 1}; //adjust this array to modify sequence of modes - as written, change to {0, 1, 2, 3} to access all modes
+int inputModeIndex = 0;
+int modeArrayLength = (sizeof(modeArray) / sizeof(modeArray[0]));
 
 // Encoder setup =============================================
 
@@ -109,7 +110,7 @@ void setup() {
   display.setTextSize(3); 
 
 //setup keyboard and mouse input
-//perhaps add in a delay so that you can program before this starts up
+//perhaps cound add in a delay so that you can program before this starts up
 Mouse.begin();
 Keyboard.begin();
 //BootKeyboard.begin(); - BootKeyboard use appears to give problems w/ Macintosh
@@ -150,48 +151,38 @@ void loop() {
     oldPosition = myEnc.read();
   }
 
-  //oldPosition = newPosition;
-    
-  //delay(100); **need a better way to debounce, or to send a single keystroke - maybe sense
-  //when it's "off" as well and wait for next cycle
-  //have some sort of secondary oldPosition comparison?
+//=========change mode=================
+
+   if ((SW6 == 0) && (SW5 == 0)){ 
+      if (inputModeIndex < modeArrayLength){
+        inputModeIndex++;
+        inputMode = modeArray[inputModeIndex];
+      }
+      if (inputModeIndex == modeArrayLength){
+        inputModeIndex = 0;
+        inputMode = modeArray[inputModeIndex];
+      }
+      SW6 = 1;
+      SW5 = 1;
+      delay(250);
+    }
 
 //================================
 
-screen();
+screen(); //need to change to only call within functions
 
 //======select input mode:=======
 
 if (inputMode == 0) volume();
 if (inputMode == 1) jiggler();
+if (inputMode == 2) slitherIO();
+if (inputMode == 3) FCPX();
 
 //Serial.println(inputMode);
 
 }
 
-
-void screen(){
-  display.clearDisplay();
-  display.invertDisplay(0);
-  display.setCursor(0,10);
-  display.print(increment);
-  display.print(decrement);
-  display.print(" ");
-  display.print(newPosition);
-  display.println(LEDLight);
-  display.print(SW1);
-  display.print(SW2);
-  display.print(SW3);
-  display.print(SW4);
-  display.print(SW5);
-  display.print(SW6);
-  display.print(inputMode);
-  display.display();
-  //Serial.println(SW1);
-  //delay(10);
-}
-
-void volume(){
+void volume(){ //works with new code
 
 //====Pixels indicate input mode==============
 
@@ -251,14 +242,9 @@ void volume(){
         Consumer.write(MEDIA_PREVIOUS);
         delay(50);
       }
-  if (SW1 == 0){ 
-  inputMode = 1;
-  SW1 = 1;
-  delay(200);
-  }
 }
 
-void jiggler(){
+void jiggler(){ //works with new code
   Serial.print("commence to jiggling");
       //Consumer.write(MEDIA_VOLUME_UP);
       //Consumer.write(MEDIA_VOLUME_DOWN);
@@ -275,15 +261,162 @@ void jiggler(){
       pixels.setPixelColor(1, pixels.Color(yMap, zMap, xMap));
       pixels.setPixelColor(3, pixels.Color(xMap, zMap, yMap));           
       pixels.show(); // Show results
-      if (SW1 == 0){ 
-      pixels.clear();
-      for(int i=0; i<NUMPIXELS; i++){
-      pixels.setPixelColor(i, pixels.Color(10, 0, 0));
-    }
-      pixels.show(); // Show results
-      inputMode = 0;
-      delay(200);
-    }
+      
 }
 
-//void FCPX() mode
+void slitherIO(){ //works with new code
+//movement tracking
+int xMovements[] = {-3, -8, -14, -16, -19, -19, -16, -14, -8, -3, 3, 8, 14, 16, 19, 19, 16, 14, 8, 3, -3};
+int yMovements[] = {19, 16, 14, 8, 3, -3, -8, -14, -16, -19, -19, -16, -14, -8, -3, 3, 8, 14, 16, 19, 19};
+int multiplier = 2; //changes speed of rotation
+
+//homing routine =====================================================  
+  if (SW6 == 0){
+    Mouse.move(40, 10); //was 60, 0 before
+    counter = 0;
+    delay(250);
+  }
+
+//mouse move routine==================================================
+
+  if (increment == 1) {
+    //need a routine to loop back to zero etc to loop around the x/y movements for each increment
+
+      for(int i=0; i< multiplier; i++){
+        if (counter == 20) counter = 1;
+        else if (counter < 20) ++counter;
+        Mouse.move(xMovements[counter], yMovements[counter]);
+        
+        Serial.print(counter); Serial.print(" "); Serial.print(xMovements[counter]); Serial.print(" "); Serial.println(yMovements[counter]);
+        increment = 0;
+        decrement = 0;
+      }
+  }
+  
+
+    if (decrement == 1) { //increment and decrement work fine individually, but don't line up when reversed
+      for(int i=0; i< multiplier; i++){     
+        if (counter == 0) counter = 19; 
+        else if (counter > 0) --counter;       
+        Mouse.move(-xMovements[counter+1], -yMovements[counter+1]);
+
+        Serial.print(counter); Serial.print(" "); Serial.print(-xMovements[counter+1]); Serial.print(" "); Serial.println(-yMovements[counter+1]);
+        increment = 0;
+        decrement = 0;
+      }
+  }
+
+//worm speed input==============================
+
+if ((SW2 == 0)||(SW3 == 0)||(SW4 ==0)){
+        Keyboard.press(HID_KEYBOARD_SPACEBAR);       
+        delay(5);
+}
+
+if ((SW2 == 1) && (SW3 == 1) && (SW4 == 1)){
+        Keyboard.releaseAll();
+        delay(5);
+}
+
+//worm auto turn input - NASCAR mode: all left turns
+
+if ((SW5 == 0) && (nascar == 0)){
+  nascar = 1;
+  delay(20);
+}
+
+else if ((SW5 == 0) && (nascar ==1)){
+  nascar = 0;
+  delay(20);
+}
+
+if (nascar == 1){
+  newNascarTurnTime = millis();
+  if ((newNascarTurnTime - oldNascarTurnTime) > 200){
+    decrement = 1;
+    oldNascarTurnTime = newNascarTurnTime;
+  }
+}
+
+
+//switch mode routine==================================================
+  
+}
+
+void FCPX(){ //works with new code
+  
+  if (increment == 1) {
+        //Keyboard.press(KEY_K);
+        //Keyboard.releaseAll();       
+        Keyboard.press(HID_KEYBOARD_RIGHTARROW);
+        Keyboard.releaseAll();
+        increment = 0;
+        decrement = 0;
+        //delay(50);
+      }
+      
+  else if (decrement == 1) {
+        //Keyboard.press(KEY_K);
+        //Keyboard.releaseAll();  
+        Keyboard.press(HID_KEYBOARD_LEFTARROW);
+        Keyboard.releaseAll();
+        increment = 0;
+        decrement = 0;
+        //delay(50);
+      }
+  else if (SW6 == 0){ //Zoom in
+        Keyboard.press(KEY_LEFT_WINDOWS);
+        Keyboard.press(HID_KEYBOARD_EQUALS_AND_PLUS);    
+        Keyboard.releaseAll();
+        delay(50);
+      }
+  else if (SW5 == 0){ //Zoom out
+        Keyboard.press(KEY_LEFT_WINDOWS);
+        Keyboard.press(HID_KEYBOARD_MINUS_AND_UNDERSCORE);    
+        Keyboard.releaseAll();
+        delay(50);
+      }
+  else if (SW4 == 0) { //fwd
+        Keyboard.press(KEY_L);
+        Keyboard.releaseAll();
+        delay(50);
+      }
+  else if (SW3 == 0) { //stop
+        Keyboard.press(KEY_K);
+        Keyboard.releaseAll();
+        delay(50);
+      }
+  else if (SW2 == 0) { //BACK
+        Keyboard.press(KEY_J);
+        Keyboard.releaseAll();
+        delay(50);
+      }
+  else if (SW1 == 0) { //break all win + shift + b
+        Keyboard.press(KEY_LEFT_WINDOWS);        
+        Keyboard.press(KEY_LEFT_SHIFT);
+        Keyboard.press(KEY_B);        
+        Keyboard.releaseAll();
+        delay(50);
+      }
+}
+
+void screen(){
+  display.clearDisplay();
+  display.invertDisplay(0);
+  display.setCursor(0,10);
+  display.print(increment);
+  display.print(decrement);
+  display.print(" ");
+  display.print(newPosition);
+  display.println(LEDLight);
+  display.print(SW1);
+  display.print(SW2);
+  display.print(SW3);
+  display.print(SW4);
+  display.print(SW5);
+  display.print(SW6);
+  display.print(inputMode);
+  display.display();
+  //Serial.println(SW1);
+  //delay(10);
+}
